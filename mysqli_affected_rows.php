e6b614aa40b410b7039e03af46708df00a080b38
dcqgit666 <1079257074@qq.com>
2017-01-18 00:20:36 +0800

    update1

--- mysqli_affected_rows.php
+++ mysqli_affected_rows.php
@@ -4,6 +4,6 @@ $con=mysqli_connect('localhost','root','');
 $sel=mysqli_select_db($con,'test');
 mysqli_query($con,"set names 'utf8'");
 
-$query=mysqli_query($con,'update test set num=20 where id =1');
+$query=mysqli_query($con,'update test set num=80 where id =1');
 
 mysqli_close($con);

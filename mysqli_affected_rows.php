a445c557e76f99f54f3898ecb24d5cfdbf08a6ab
dcqgit666 <1079257074@qq.com>
2017-01-18 00:33:44 +0800

    insert

--- mysqli_affected_rows.php
+++ mysqli_affected_rows.php
@@ -4,11 +4,19 @@ $con=mysqli_connect('localhost','root','');
 $sel=mysqli_select_db($con,'test');
 mysqli_query($con,"set names 'utf8'");
 
-if(mysqli_query($con,'update test set num=80 where id =1')){
-	echo "更新成功，更新数据为";
+// if(mysqli_query($con,'update test set num=80 where id =1')){
+// 	echo "更新成功，更新数据为";
+// 	echo mysqli_affected_rows($con);
+// }else{
+// 	echo "更新失败";
+// }
+
+if(mysqli_query($con,'insert into test(name,num,price) values("西红柿",10,8)')){
+	echo "插入成功，插入数据为";
 	echo mysqli_affected_rows($con);
 }else{
-	echo "更新失败";
+	echo mysqli_error($con);
+	echo "插入失败";
 }
 
 mysqli_close($con);

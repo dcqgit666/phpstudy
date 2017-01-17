0234382ce2ccf21f0a3443bd460838b9332204de
dcqgit666 <1079257074@qq.com>
2017-01-18 00:03:56 +0800

    update

--- mysqli_affected_rows.php
+++ mysqli_affected_rows.php
@@ -4,12 +4,6 @@ $con=mysqli_connect('localhost','root','');
 $sel=mysqli_select_db($con,'test');
 mysqli_query($con,"set names 'utf8'");
 
-// $query=mysqli_query($con,'select * from test');
-// $num=mysqli_num_rows($query);
-// echo $num;
-
-$query=mysqli_query($con,'select count(*) from test');
-$num=mysqli_fetch_row($query);
-echo $num[0]."<br>";
+$query=mysqli_query($con,'update test set num=20 where id =1');
 
 mysqli_close($con);

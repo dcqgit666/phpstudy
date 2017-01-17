351bcb402aefdd22b148abc451d136af7bc355ec
dcqgit666 <1079257074@qq.com>
2017-01-17 23:33:40 +0800

    add mysqli

---
+++ mysqli_result.php
@@ -0,0 +1,18 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+$con=mysqli_connect('localhost','root','');
+$sel=mysqli_select_db($con,'test');
+mysqli_query($con,"set names 'utf8'");
+
+// $query=mysqli_query($con,'select * from test');
+// $num=mysqli_num_rows($query);
+// echo $num;
+
+// $query=mysqli_query($con,'select count(*) from test');
+// $num=mysqli_fetch_row($query);
+// echo $num[0];
+
+$query=mysqli_query($con,'select count(*) from test');
+echo mysqli_result($query,0);
+
+mysqli_close($con);

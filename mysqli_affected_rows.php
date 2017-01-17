bd982a33254ccc4002a9ed3daa09b827523ed2a7
dcqgit666 <1079257074@qq.com>
2017-01-18 00:25:12 +0800

    update if

--- mysqli_affected_rows.php
+++ mysqli_affected_rows.php
@@ -4,6 +4,11 @@ $con=mysqli_connect('localhost','root','');
 $sel=mysqli_select_db($con,'test');
 mysqli_query($con,"set names 'utf8'");
 
-$query=mysqli_query($con,'update test set num=80 where id =1');
+if(mysqli_query($con,'update test set num=80 where id =1')){
+	echo "更新成功，更新数据为";
+	echo mysqli_affected_rows($con);
+}else{
+	echo "更新失败";
+}
 
 mysqli_close($con);

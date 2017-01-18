98701df8722fc73709b36c868d04fa247b32218b
dcqgit666 <1079257074@qq.com>
2017-01-18 21:47:21 +0800

    edit connedt

---
+++ article/connect.php
@@ -0,0 +1,15 @@
+<?php
+	require_once("config.php");//引入配置文件
+	// 连接数据库
+	$con=mysqli_connect(HOST,USERNAME,PASSWORD);
+	if (!$con) {
+		echo mysqli_error($con);
+	}
+	// 选择数据库
+	if (!mysqli_select_db($con,"test")) {
+		echo mysqli_error($con);
+	}
+	// 设置字符集
+	if (mysqli_query($con,'set names utf8');) {
+		echo mysqli_error($con);
+	}
